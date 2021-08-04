9d1681b79821329b8d889bec81bfcd2c57e2d105
user.name=chenxiangwen1 <chenxiangwen@4paradigm.com>
2021-08-04 19:21:56 +0800

    separate portal

--- src/portal/src/constant/task.ts
+++
@@ -1,8 +0,0 @@
-export enum Task {
-  Succeed = 'Succeed',
-  Stopped = 'Stopped',
-  InProgress = 'InProgress',
-  Pending = 'Pending',
-  Failed = 'Failed',
-  NotFound = 'NotFound',
-}
--- src/portal/src/pages/Home/data/games/intel-ai-apps-20210601/README.md
+++
@@ -1,3 +0,0 @@
-# [ai-apps] AI 应用与异构内存编程挑战赛 - 应用闯关赛道
-
-[☞ 点此阅读赛题](https://memark.io/ai_2021/eval_track.html)
